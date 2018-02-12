68cfdb1081a6273abf4e8b7bca256aef1ba65c95
HEMBERT Luciana Ext DTSI/DERS <SHXG0185@orange.com>
2018-02-12 14:29:24 +0100

    teste

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -20,6 +20,7 @@ class GenusController extends Controller
     {
 		
 		//j'ai ajouté cette ligne ici
+		//deuixième ligne
         $genus = new Genus();
         $genus->setName('Octopus'.rand(1, 100));
         $genus->setSubFamily('Octopodinae');
